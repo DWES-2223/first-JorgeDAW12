b7c0d07dc629bdcb748423c6c7f8b42561110a1a
Jorge <kokekola12@gmail.com>
2023-09-25 18:23:26 +0200

    205.php

--- php/src/205.php
+++ php/src/205.php
@@ -1,66 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
 <?php
-/*
-$quantitat = $_GET["quantitat"];
+extract($_GET);
+$bitlletsDescomposicio = [500, 200, 100, 50, 20, 10, 5, 2, 1];
+?>
 
-$billets500 = 0;
-$billets200 = 0;
-$billets100 = 0;
-$billets50 = 0;
-$billets20 = 0;
-$billets10 = 0;
-$billets5 = 0;
-$monedaDeEuro = 0;
-$monedaDeDosEuros = 0;
-
-do {
-    if($quantitat >= 500) {
-        $billets500 += 1;
-        $quantitat - 500;
-        if($quantitat >= 200) {
-            $billets200 += 1;
-            $quantitat - 200;
-            if($quantitat >= 100) {
-                $billets100 += 1;
-                $quantitat - 100;
-                if($quantitat >= 50) {
-                    $billets50 += 1;
-                    $quantitat - 50;
-                    if($quantitat >= 20) {
-                        $billets20 += 1;
-                        $quantitat - 20;
-                        if($quantitat >= 10) {
-                            $billets10 += 1;
-                            $quantitat - 10;
-                            if($quantitat >= 5) {
-                                $billets5 += 1;
-                                $quantitat - 5;
-                                if($quantitat >= 2) {
-                                    $monedaDeDosEuros += 1;
-                                    $quantitat - 2;
-                                    if($quantitat >= 1) {
-                                        $monedaDeEuro += 1;
-                                        $quantitat - 1;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+<head>
+    <meta charset="UTF-8">
+    <title>Diners</title>
+</head>
+<body>
+<?php
+if (isset($quantitat)) {
+    foreach ($bitlletsDescomposicio as $bitllet) {
+        $numeroBitllets = intdiv($quantitat, $bitllet);
+        $quantitatRecaudada = $bitllet * $numeroBitllets;
+        $tipusBitllet = ($numeroBitllets > 1) ? (($bitllet > 2) ? "bitllets" : "monedes") :
+            (($bitllet > 2) ? "bitllet" : "moneda");
+        echo "$numeroBitllets $tipusBitllet de $bitllet <br>";
+        $quantitat -= $quantitatRecaudada;
     }
-} while($quantitat >= 0);
-
-if(isset($quantitat)) {
-    echo "$billets500 billet de 500<br>";
-    echo "$billets200 billet de 200<br>";
-    echo "$billets100 billet de 100<br>";
-    echo "$billets50 billet de 50<br>";
-    echo "$billets20 billet de 20<br>";
-    echo "$billets10 billet de 10<br>";
-    echo "$billets5 billet de 5<br>";
-    echo "$monedaDeDosEuros moneda de 2<br>";
-    echo "$monedaDeEuro moneda de 1";
 } else {
     echo "Posa la quantitat a la variable quantitat pel QueryString";
-}*/
+}
+?>
+</body>
+</html>
